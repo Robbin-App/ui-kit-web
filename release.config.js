0a3b6aa5698ae3c471ae15dcc1e07e37269483de
José Ignacio Guerrero VInueza <josevinguerrero@gmail.com>
2021-06-04 01:35:59 -0500

    fix: 🐛 Add missing deployment script

--- release.config.js
+++ release.config.js
@@ -5,5 +5,19 @@ module.exports = {
     '@semantic-release/github',
     '@semantic-release/npm',
     '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'docs/CHANGELOG.md',
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['docs', 'package.json'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
   ],
 };
