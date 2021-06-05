1078ad65b9c13ec0b291a860f5e09c3519a0587c
José Ignacio Guerrero VInueza <josevinguerrero@gmail.com>
2021-06-05 18:28:25 -0500

    refactor: 💡 Abstract FormGroup component

--- src/components/FormInput/index.tsx
+++ src/components/FormInput/index.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import styled from 'styled-components';
-import { getRem } from '../../helpers';
+import { FormGroup } from '../FormGroup';
 
 export interface IFormInputProps {
   /**
@@ -70,94 +69,29 @@ export const FormInput: React.FunctionComponent<
     if (!isFocus) setUseMiniLabel(String(value).length > 0);
   }, [value]);
   return (
-    <FormGroup className={className}>
-      <FormField {...{ error, useMiniLabel, isFocus, disabled }}>
-        <label htmlFor={formId}>{label}</label>
-        <input
-          {...props}
-          ref={inputElement}
-          placeholder=""
-          id={formId}
-          disabled={disabled}
-          value={value}
-          onChange={onChange}
-          onBlur={handleInputBlur}
-          onFocus={handleInputFocus}
-          onKeyDown={handleKeyDown}
-        />
-      </FormField>
-      {error && <small>{error}</small>}
+    <FormGroup
+      label={label}
+      formId={formId}
+      miniLabel={useMiniLabel}
+      isFocus={isFocus}
+      disabled={disabled}
+      error={error}
+      className={className}
+    >
+      <input
+        {...props}
+        ref={inputElement}
+        placeholder=""
+        id={formId}
+        disabled={disabled}
+        value={value}
+        onChange={onChange}
+        onBlur={handleInputBlur}
+        onFocus={handleInputFocus}
+        onKeyDown={handleKeyDown}
+      />
     </FormGroup>
   );
 };
-
-const FormGroup = styled.div`
-  position: relative;
-  margin-bottom: 12px;
-  small {
-    position: absolute;
-    width: 100%;
-    top: calc(100% + 4px);
-    left: 0;
-    display: block;
-    font-size: ${getRem(12)};
-    line-height: ${getRem(12)};
-    color: ${({ theme }) => theme.colors.error};
-  }
-`;
-
-const FormField = styled.div<{
-  isFocus: boolean;
-  useMiniLabel: boolean;
-  disabled?: boolean;
-  error?: string | boolean;
-}>`
-  position: relative;
-  label {
-    display: flex;
-    align-items: center;
-    position: absolute;
-    left: 14px;
-    color: ${(props) =>
-      props.error
-        ? props.theme.colors.error
-        : props.disabled
-        ? props.theme.colors.text.disabledText
-        : props.theme.colors.text.globalText};
-    transform: translateY(${(props) => (props.useMiniLabel ? '6px' : 0)});
-    top: 0;
-    transition: transform 0.3s ease-out;
-    bottom: ${(props) => (props.useMiniLabel ? 'auto' : 0)};
-    font-size: ${(props) => (props.useMiniLabel ? getRem(10) : getRem(16))};
-    line-height: ${(props) => (props.useMiniLabel ? getRem(12) : getRem(20))};
-    font-weight: ${(props) =>
-      props.useMiniLabel
-        ? props.theme.fonts.weight.bold
-        : props.theme.fonts.weight.regular};
-    margin: auto 0;
-    pointer-events: none;
-  }
-  input {
-    font-size: ${getRem(16)};
-    line-height: ${getRem(20)};
-    padding: 22px 14px 10px;
-    transition: all 0.3s ease-in;
-    color: ${({ theme }) => theme.colors.text.globalText};
-    border-radius: ${({ theme }) => theme.sizes.borderRadius};
-    border: 1px solid
-      ${({ theme, error }) =>
-        error ? theme.colors.error : theme.colors.darkBorder};
-    background-color: ${({ theme }) => theme.colors.white};
-    &:focus {
-      outline: none;
-      border-color: ${({ theme, error }) =>
-        error ? theme.colors.error : theme.colors.primary90};
-    }
-    &:disabled {
-      color: ${({ theme }) => theme.colors.text.disabledText};
-      border-color: ${({ theme }) => theme.colors.text.disabledText};
-    }
-  }
-`;
 
 export default FormInput;
