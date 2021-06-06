1611b081048762314398b18639d1701a7c0a77b2
José Ignacio Guerrero VInueza <josevinguerrero@gmail.com>
2021-06-05 19:20:10 -0500

    style: 💄 Remove unused imports

--- src/components/FormInput/index.tsx
+++ src/components/FormInput/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import { FormGroup } from '../FormGroup';
 import { useTextInputMiniLabel } from '../../hooks/useTextInputMiniLabel';
 
