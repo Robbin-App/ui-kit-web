44d83bca0e67f9dff8c09fedc52f2e211b4a247d
José Ignacio Guerrero VInueza <josevinguerrero@gmail.com>
2021-06-09 23:23:19 -0500

    fix: 🐛 Add missing FormCheckbox component

---
+++ src/components/FormCheckbox/FormCheckbox.test.tsx
@@ -0,0 +1,20 @@
+import { ThemeProvider } from 'styled-components';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { baseRobbinTheme } from '../../styles';
+import FormCheckbox from './index';
+
+const renderPropComponent = (theme, props = {}) => (
+  <ThemeProvider theme={theme}>
+    <FormCheckbox label="test label" formId="test-id" {...props} />
+  </ThemeProvider>
+);
+
+describe('FormCheckbox', () => {
+  test('renders', () => {
+    const render = renderer
+      .create(renderPropComponent(baseRobbinTheme))
+      .toJSON();
+    expect(render).toMatchSnapshot();
+  });
+});
---
+++ src/components/FormCheckbox/index.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import styled from 'styled-components';
+import Check from '@material-ui/icons/Check';
+
+export interface IFormCheckboxProps {
+  /**
+   * Label for the FormCheckbox, use children if need to render custom markup
+   */
+  label?: string;
+  /**
+   * FormId of the FormCheckbox
+   */
+  formId: string;
+  /**
+   * Error state of the FormCheckbox
+   */
+  error?: string | boolean;
+  /**
+   * Disable the FormCheckbox
+   */
+  disabled?: boolean;
+  /**
+   * ClassName of the FormCheckbox
+   */
+  className?: string;
+}
+
+/**
+ * Basic Form Checkbox Component
+ */
+const FormCheckbox: React.FunctionComponent<
+  IFormCheckboxProps & React.InputHTMLAttributes<HTMLInputElement>
+> = ({ children, formId, label, ...props }) => {
+  return (
+    <StyledLabel htmlFor={formId}>
+      {children || label}
+      <input {...props} type="checkbox" id={formId} />
+      <span>
+        <Check />
+      </span>
+    </StyledLabel>
+  );
+};
+
+const StyledLabel = styled.label`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  position: relative;
+  padding-left: 36px;
+  input {
+    display: none;
+    &:checked ~ span {
+      background: ${(props) => props.theme.colors.primary100};
+      border-color: ${(props) => props.theme.colors.primary100};
+      svg {
+        display: block;
+      }
+    }
+  }
+  span {
+    position: absolute;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    margin: auto 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 24px;
+    height: 24px;
+    border-radius: 4px;
+    border: 1px solid ${(props) => props.theme.colors.border.darkBorder};
+    svg {
+      display: none;
+      color: ${(props) => props.theme.colors.white};
+    }
+  }
+`;
+
+export default FormCheckbox;
